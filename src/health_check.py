f547a78d64c9fa9004358cf110d9fbdba37fa4e8
Parth Lawania <parthlawania@gmail.com>
2023-12-12 13:49:12 +0530

    serverless config and health check api

--- src/health_check.py
+++
@@ -1,9 +0,0 @@
-import json
-def health_check(event,context):
-    print(json.dumps(event))
-    response = {
-            "statusCode":200,
-            "headers":{},
-            "body":json.dumps({"message":"Hello World"})
-            }
-    return response
---
+++ src/health_check/handler.py
@@ -0,0 +1,9 @@
+import json
+def main(event,context):
+    print(json.dumps(event))
+    response = {
+            "statusCode":200,
+            "headers":{},
+            "body":json.dumps({"message":"Hello World"})
+            }
+    return response
